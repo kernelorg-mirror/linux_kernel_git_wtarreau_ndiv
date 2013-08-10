f74deecbacb51159318d223071a096e643fb1b0b
Willy Tarreau <w@1wt.eu>
2013-08-10 14:50:05 +0200

    WIP: capture works at high rate to RAM but crashes to disk
    Logging to RAM disk is OK, but logging to disk forces scheduling (at least
    to expand the file) and crashes :
    
    BUG: scheduling while atomic: swapper/0/0/0x00000100
    CPU: 0 PID: 0 Comm: swapper/0 Tainted: G         C   3.11.0-rc4-mvebu-thm #2
    [<c0011925>] (unwind_backtrace+0x1/0x98) from [<c000eff7>] (show_stack+0xb/0xc)
    [<c000eff7>] (show_stack+0xb/0xc) from [<c02a141b>] (dump_stack+0x4f/0x64)
    [<c02a141b>] (dump_stack+0x4f/0x64) from [<c029fecb>] (__schedule_bug+0x33/0x48)
    [<c029fecb>] (__schedule_bug+0x33/0x48) from [<c02a324b>] (__schedule+0x30f/0x3d4)
    [<c02a324b>] (__schedule+0x30f/0x3d4) from [<c02a340d>] (io_schedule+0x4d/0x74)
    [<c02a340d>] (io_schedule+0x4d/0x74) from [<c00987db>] (sleep_on_buffer+0x7/0xc)
    [<c00987db>] (sleep_on_buffer+0x7/0xc) from [<c02a1b9d>] (__wait_on_bit+0x59/0x80)
    [<c02a1b9d>] (__wait_on_bit+0x59/0x80) from [<c02a1c07>] (out_of_line_wait_on_bit+0x43/0x48)
    [<c02a1c07>] (out_of_line_wait_on_bit+0x43/0x48) from [<c009a6fd>] (bh_submit_read+0x59/0x60)
    [<c009a6fd>] (bh_submit_read+0x59/0x60) from [<c00b9709>] (read_block_bitmap+0x45/0xe4)
    [<c00b9709>] (read_block_bitmap+0x45/0xe4) from [<c00ba03d>] (ext3_new_blocks+0x125/0x3dc)
    [<c00ba03d>] (ext3_new_blocks+0x125/0x3dc) from [<c00bde39>] (ext3_get_blocks_handle+0x195/0x6f8)
    [<c00bde39>] (ext3_get_blocks_handle+0x195/0x6f8) from [<c00be417>] (ext3_get_block+0x7b/0xa8)
    [<c00be417>] (ext3_get_block+0x7b/0xa8) from [<c0099f1f>] (__block_write_begin+0x11f/0x2a0)
    [<c0099f1f>] (__block_write_begin+0x11f/0x2a0) from [<c00bda47>] (ext3_write_begin+0x83/0x160)
    [<c00bda47>] (ext3_write_begin+0x83/0x160) from [<c005d9bb>] (pagecache_write_begin+0x1b/0x20)
    [<c005d9bb>] (pagecache_write_begin+0x1b/0x20) from [<bf8002cf>] (map_curr_page.part.1+0x3a/0x7c [ethtap])
    unwind: Index not found bf8002cf
    bad: scheduling from the idle thread!
    
    So we'll likely need to have a dual-buffer area from which a thread will
    move pages to the page cache (but how ?).

--- drivers/staging/ethtap/ethtap.c
+++ drivers/staging/ethtap/ethtap.c
@@ -15,17 +15,26 @@
 #include <linux/fs.h>
 #include <linux/namei.h>
 #include <linux/mman.h>
+#include <linux/pagemap.h>
 
 static int every = 16384;
 static char *dev = "";
 static char *dump = "";
+static int pol = ETHDIV_RX_PASS;
 
-static struct file *file = NULL;
-static unsigned long map = 0;
+//static unsigned long map = 0;
+
+static struct page *curr_page = NULL;
+static loff_t curr_pos = 0;
+static loff_t prev_pos = 0;
+static char *curr_ptr = NULL;
+static void *curr_fsdata = NULL;
+static struct file *file;
 
 module_param(every, int, 0644);  MODULE_PARM_DESC(every, "print a dump this every packet count (power of two)");
 module_param(dev, charp, 0644);  MODULE_PARM_DESC(dev, "Interface name to attach to");
 module_param(dump, charp, 0644); MODULE_PARM_DESC(dump, "File name to dump the output to");
+module_param(pol, int, 0644);    MODULE_PARM_DESC(pol, "Policy on captured packets (0=pass, 1=drop)");
 
 /* unfortunately, none of the truncate flavors are exported, so we have to
  * do it ourselves :-(
@@ -34,7 +43,6 @@ struct file *create_file(const char *name, loff_t size)
 {
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 	struct path path;
-	struct file *file;
 	int error;
 	mm_segment_t fs;
 
@@ -44,6 +52,9 @@ struct file *create_file(const char *name, loff_t size)
 		printk(KERN_DEBUG "Creation of file <%s> failed with error %d\n", name, error);
 		return NULL;
 	}
+	printk(KERN_DEBUG "file %s opened\n", name);
+
+	/*
  retry:
 	fs = get_fs();
 	set_fs(KERNEL_DS);
@@ -64,8 +75,49 @@ struct file *create_file(const char *name, loff_t size)
                 filp_close(file, NULL);
 		file = NULL;
 	}
-	printk(KERN_DEBUG "file=%p (name=%s)\n", file, name);
+	*/
 	return file;
+}
+
+static int map_curr_page(void)
+{
+	int error;
+
+	if (curr_page)
+		return 0;
+
+	printk(KERN_DEBUG "file=%p mapping=%p\n", file, file->f_mapping);
+	error = pagecache_write_begin(file, file->f_mapping, curr_pos, PAGE_CACHE_SIZE - (curr_pos & ~PAGE_CACHE_MASK),
+	                              AOP_FLAG_UNINTERRUPTIBLE, &curr_page, &curr_fsdata);
+	prev_pos = curr_pos;
+	printk(KERN_DEBUG "write_begin done, curr_page=%p, _count=%d\n", curr_page, curr_page->_count);
+	if (!error) {
+		curr_ptr = kmap_atomic(curr_page);
+		printk(KERN_DEBUG "dst=%p, _count=%d\n", curr_ptr, curr_page->_count);
+	}
+	return error;
+}
+
+static int unmap_curr_page(void)
+{
+	int error;
+
+	if (!curr_page)
+		return 0;
+
+	if (curr_ptr) {
+		kunmap_atomic(curr_ptr);
+		curr_ptr = NULL;
+	}
+	error = pagecache_write_end(file, file->f_mapping, prev_pos, PAGE_CACHE_SIZE - (prev_pos & ~PAGE_CACHE_MASK), curr_pos - prev_pos,
+				    curr_page, curr_fsdata);
+	printk(KERN_DEBUG "write_end done, %d->%d = %d bytes\n", (int)prev_pos, (int)curr_pos, (int)curr_pos - (int)prev_pos);
+	printk(KERN_DEBUG "curr_page=%p, _count=%d\n", curr_page, curr_page->_count);
+
+	printk(KERN_DEBUG "page=%p fsdata=%p error=%d\n", curr_page, curr_fsdata, error);
+
+	curr_page = NULL;
+	return error;
 }
 
 static int
@@ -88,13 +140,13 @@ handle_device_event(struct notifier_block *notif, unsigned long event, void *ptr
 	return NOTIFY_DONE;
 }
 
-static enum ethdiv_rx_status print_pkt(struct ethdiv *ethdiv, const char *data, int len, char **resp, int *rlen)
+static enum ethdiv_rx_status handle_rx(struct ethdiv *ethdiv, const char *data, int len, char **resp, int *rlen)
 {
 	static int pkt_cnt[CONFIG_NR_CPUS];
 	int idx = smp_processor_id();
+	int len1, len2;
 
 	pkt_cnt[idx]++;
-
 	if ((pkt_cnt[idx] & (every - 1)) == 0) {
 		printk(KERN_DEBUG "%s: ethdiv=%p d=%p l=%d cpu=%d cnt=%d: %02x%02x:%02x%02x:%02x%02x %02x%02x:%02x%02x:%02x%02x %02x%02x:%02x%02x\n",
 		       dev_name(&ethdiv->dev->dev),
@@ -103,14 +155,45 @@ static enum ethdiv_rx_status print_pkt(struct ethdiv *ethdiv, const char *data, 
 		       data[4], data[5], data[6], data[7],
 		       data[8], data[9], data[10], data[11],
 		       data[12], data[13], data[14], data[15]);
-		memcpy((void *)map, data, len);
 	}
 
-	return ETHDIV_RX_PASS;
+	/* capture packets. They're assumed to be smaller than PAGE_CACHE_SIZE */
+	if (!curr_page)
+		map_curr_page();
+
+	/* now let's copy the current block in one or two pages */
+	len1 = PAGE_CACHE_SIZE - (curr_pos & ~PAGE_CACHE_MASK);
+	len2 = len - len1;
+
+	if (len2 >= 0) {
+		/* we have to copy len1 in the first page, then possibly len2 */
+		if (curr_page)
+			memcpy(curr_ptr + (curr_pos & ~PAGE_CACHE_MASK), data, len1);
+		curr_pos += len1;
+		unmap_curr_page();
+		map_curr_page();
+	}
+	else {
+		/* copy everything at once */
+		len2 = len;
+	}
+
+	/* len2 here can never reach the end of the current page */
+	if (curr_page && len2)
+		memcpy(curr_ptr + (curr_pos & ~PAGE_CACHE_MASK), data, len2);
+	curr_pos += len2;
+
+	return pol;
+}
+
+static void rx_done(struct ethdiv *ethdiv)
+{
+	unmap_curr_page();
 }
 
 static struct ethdiv ethdiv = {
-	.handle_rx = print_pkt,
+	.handle_rx = handle_rx,
+	.rx_done   = rx_done,
 };
 
 struct notifier_block notifier = {
@@ -131,18 +214,17 @@ static int __init modinit(void)
 	register_netdevice_notifier(&notifier);
 	printk(KERN_DEBUG "Attached to device %s\n", ethdiv.dev->name);
 
-	file = create_file(dump, 1048576);
-	if (!file)
+	if (!create_file(dump, 1048576))
 		goto fail_open;
 
-	map = vm_mmap(file, 0, 1048576, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_POPULATE, 0);
-	printk(KERN_DEBUG "mapped to address %lx, file->f_mapping=%p\n", map, file->f_mapping);
-
-	if (map > -1024UL) {
-		/* error */
-		ret = (signed)map;
-		goto fail_map;
-	}
+	//map = vm_mmap(file, 0, 1048576, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_POPULATE, 0);
+	//printk(KERN_DEBUG "mapped to address %lx, file->f_mapping=%p\n", map, file->f_mapping);
+	//
+	//if (map > -1024UL) {
+	//	/* error */
+	//	ret = (signed)map;
+	//	goto fail_map;
+	//}
         return 0;
 
  fail_map:
@@ -157,8 +239,9 @@ static int __init modinit(void)
 static void __exit modexit(void)
 {
 	if (file) {
-		if (file->f_mapping)
-			unmap_mapping_range(file->f_mapping, 0, 1048576, 1);
+		//if (file->f_mapping)
+		//	unmap_mapping_range(file->f_mapping, 0, 1048576, 1);
+		unmap_curr_page();
                 filp_close(file, NULL);
 		file = NULL;
 	}
