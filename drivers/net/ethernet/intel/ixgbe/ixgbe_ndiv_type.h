bee9e690f13e9b358147146601a728126832dc9c
Emeric Brun <ebrun@haproxy.com>
2014-04-11 08:07:25 -0500

    ixgbe: ndiv: review ndiv descripors allocation to use a single dma map and to separate descriptors and data buffers.

--- drivers/net/ethernet/intel/ixgbe/ixgbe_ndiv_type.h
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_ndiv_type.h
@@ -6,13 +6,14 @@ struct ixgbe_ndiv_rdesc {
 	u32 len;
 	u16 csum_off;
 	u16 l4_off;
-	u8 unused[NET_IP_ALIGN];
-	u8 data[1500]; /* set to def MTU */
+	u8 *data;
 };
 
 struct ixgbe_ndiv_rsp {
 	struct ixgbe_ndiv_rdesc *descs;
 	u16 size;
+	u8 *data;
+	dma_addr_t dma;
 	u16 avail;
 	u16 pending;
 	u16 next_to_send;
