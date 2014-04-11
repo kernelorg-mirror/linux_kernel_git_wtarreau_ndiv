b14897f108272228deded8599a8f9e90c80ee748
Emeric Brun <ebrun@haproxy.com>
2014-04-11 08:00:21 -0500

    ixgbe/ndiv: Rise tx work limit from 256 to 4096, to be sure all free tx descriptors cleaned on a poll loop.

--- drivers/net/ethernet/intel/ixgbe/ixgbe.h
+++ drivers/net/ethernet/intel/ixgbe/ixgbe.h
@@ -66,7 +66,7 @@
 
 /* TX/RX descriptor defines */
 #define IXGBE_DEFAULT_TXD		    512
-#define IXGBE_DEFAULT_TX_WORK		    256
+#define IXGBE_DEFAULT_TX_WORK		   4096
 #define IXGBE_MAX_TXD			   4096
 #define IXGBE_MIN_TXD			     64
 
