ac3c776f8be34185e338909a7966bc3b342d530b
Emeric Brun <ebrun@haproxy.com>
2015-01-08 10:50:13 +0100

    ixgbe/ndiv: Fix infinite loop in case of full attack.

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -2125,6 +2125,7 @@ static int ixgbe_clean_rx_irq(struct ixgbe_q_vector *q_vector,
 	u16 cleaned_count = ixgbe_desc_unused(rx_ring);
 	int force_tx = q_vector->rx.ring->count >> 1;
 	bool abort = 0;
+	int  half_ring = 0;
 
 	while (likely(total_rx_packets < budget)) {
 		union ixgbe_adv_rx_desc *rx_desc;
@@ -2138,6 +2139,8 @@ static int ixgbe_clean_rx_irq(struct ixgbe_q_vector *q_vector,
 			if (q_vector->ndiv_rsp.pending)
 				ixgbe_ndiv_send_rsp(q_vector);
 			force_tx = q_vector->rx.ring->count >> 1;
+			if (++half_ring == 2)
+				break;
 		}
 
 		/* return some buffers to hardware, one at a time is too slow */
