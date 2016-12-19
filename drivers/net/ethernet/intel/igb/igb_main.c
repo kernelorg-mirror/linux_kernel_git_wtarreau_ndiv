4cc1cb2382d77fc32b8ba18b9ec004a68f41f795
Emeric Brun <ebrun@haproxy.com>
2016-12-19 15:55:37 +0100

    igb: ndiv: force queue pairing for all chips

--- drivers/net/ethernet/intel/igb/igb_main.c
+++ drivers/net/ethernet/intel/igb/igb_main.c
@@ -3816,6 +3816,9 @@ void igb_set_flag_queue_pairs(struct igb_adapter *adapter,
 			adapter->flags &= ~IGB_FLAG_QUEUE_PAIRS;
 		break;
 	}
+
+	/* Force queue pairing for ndiv framework */
+	adapter->flags |= IGB_FLAG_QUEUE_PAIRS;
 }
 
 /**
